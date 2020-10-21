e81c644eb37839a3db65ef8c2d49cb8af8bdfa70
besiansopa <71036295+besiansopa@users.noreply.github.com>
2020-10-21 23:22:51 +0200

    Update formular.php

--- formular/formular.php
+++ formular/formular.php
@@ -1,3 +1,22 @@
+Vielen Dank, f&uuml;r Ihre Nachricht!
 <?php
-echo "Vielen Dank, die informationen wurden übermittelt!";
+	
+	$firstName 		= $_POST['vorname'];
+    $lastName 		= $_POST['nachname'];
+	$email	 		= $_POST['email'];
+	$message 		= $_POST['nachricht'];
+
+	$to 			= "besian.sopa@bbzbl-it.ch";
+	$subject 		= "Email von meiner Webseite";
+	$body 			= "Informationen:";
+
+	$body .= "\r\n Vorname: " . $firstName;
+    $body .= "\r\n Nachname: " . $lastName;
+	$body .= "\r\n Email-Adresse: " . $email;
+	$body .= "\r\n Nachricht: " . $message;
+	$headers = 'From: ' . $email . "\r\n" .
+               'Reply-To: ' . $email . "\r\n" .
+               'X-Mailer: PHP/' . phpversion();
+
+	mail($to, $subject, $body);
 ?>
